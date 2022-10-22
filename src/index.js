b4d0cf04d29b2000f686e9818587927a21ce1afc
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2022-10-22 13:00:38 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,130 @@
-import './css/styles.css';
+// Завдання - пошук країн
+// Створи фронтенд частину програми пошуку даних про країну за її частковою або повною назвою. Подивися демо-відео роботи програми.
 
+// HTTP-запит
+// Використовуй публічний API Rest Countries v2, а саме ресурс name, який повертає масив об'єктів країн, що задовольнили критерій пошуку. Додай мінімальне оформлення елементів інтерфейсу.
+
+// Напиши функцію fetchCountries(name), яка робить HTTP-запит на ресурс name і повертає проміс з масивом країн - результатом запиту. Винеси її в окремий файл fetchCountries.js і зроби іменований експорт.
+
+// Фільтрація полів
+// У відповіді від бекенду повертаються об'єкти, велика частина властивостей яких, тобі не знадобиться. Щоб скоротити обсяг переданих даних, додай рядок параметрів запиту - таким чином цей бекенд реалізує фільтрацію полів. Ознайомся з документацією синтаксису фільтрів.
+
+// Тобі потрібні тільки наступні властивості:
+
+// name.official - повна назва країни
+// capital - столиця
+// population - населення
+// flags.svg - посилання на зображення прапора
+// languages - масив мов
+// Поле пошуку
+// Назву країни для пошуку користувач вводить у текстове поле input#search-box. HTTP-запити виконуються при введенні назви країни, тобто на події input. Але робити запит з кожним натисканням клавіші не можна, оскільки одночасно буде багато запитів і вони будуть виконуватися в непередбачуваному порядку.
+
+// Необхідно застосувати прийом Debounce на обробнику події і робити HTTP-запит через 300мс після того, як користувач перестав вводити текст. Використовуй пакет lodash.debounce.
+
+// Якщо користувач повністю очищає поле пошуку, то HTTP-запит не виконується, а розмітка списку країн або інформації про країну зникає.
+
+// Виконай санітизацію введеного рядка методом trim(), це вирішить проблему, коли в полі введення тільки пробіли, або вони є на початку і в кінці рядка.
+
+// Інтерфейс
+// Якщо у відповіді бекенд повернув більше ніж 10 країн, в інтерфейсі з'являється повідомлення про те, що назва повинна бути специфічнішою. Для повідомлень використовуй бібліотеку notiflix і виводь такий рядок "Too many matches found. Please enter a more specific name.".
+
+// Too many matches alert
+
+// Якщо бекенд повернув від 2-х до 10-и країн, під тестовим полем відображається список знайдених країн. Кожен елемент списку складається з прапора та назви країни.
+
+// Country list UI
+
+// Якщо результат запиту - це масив з однією країною, в інтерфейсі відображається розмітка картки з даними про країну: прапор, назва, столиця, населення і мови.
+
+// Country info UI
+
+// УВАГА
+// Достатньо, щоб застосунок працював для більшості країн. Деякі країни, як-от Sudan, можуть створювати проблеми, оскільки назва країни є частиною назви іншої країни - South Sudan. Не потрібно турбуватися про ці винятки.
+
+// Обробка помилки
+// Якщо користувач ввів назву країни, якої не існує, бекенд поверне не порожній масив, а помилку зі статус кодом 404 - не знайдено. Якщо це не обробити, то користувач ніколи не дізнається про те, що пошук не дав результатів. Додай повідомлення "Oops, there is no country with that name" у разі помилки, використовуючи бібліотеку notiflix.
+
+// Error alert
+
+// УВАГА
+// Не забувай про те, що fetch не вважає 404 помилкою, тому необхідно явно відхилити проміс, щоб можна було зловити і обробити помилку.
+
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
+
+const inputEl = document.querySelector('#search-box');
+const countriesListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+inputEl.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
+
+function onSearchCountry(evt) {
+  const name = evt.target.value;
+  clearContent();
+  if (name === '') {
+    return;
+  }
+  fetchCountries(name.trim())
+    .then(countries => {
+      clearContent();
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (countries.length > 1 && countries.length < 10) {
+        clearContent();
+        showListCountries(countries);
+      }
+      if (countries.length === 1) {
+        clearContent();
+        showMarkupCountry(countries);
+      }
+    })
+    .catch(error => {
+      clearContent();
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function renderMarkupCountryDescription(country) {
+  const languagesList = Object.values(country[0].languages);
+  return `<div class="countryName"><img class="flag" src="${
+    country[0].flags.svg
+  }" alt="${country[0].name.official}flag">
+<h1 class="country">${country[0].name.official}</h1></div>
+<p><b>Capital:</b> ${country[0].capital[0]}</p>
+<p><b>Population:</b> ${country[0].population}</p>
+<p><b>Languages:</b> ${languagesList.join(', ')}</p>`;
+}
+
+function markupCountriesList(countries) {
+  return countries
+    .map(
+      country => `<li class="description-country">
+  <img class="small-flag" src="${country.flags.svg}" alt="${country.name.official}flag">
+  <span>${country.name.official}</span>
+</li>`
+    )
+    .join('');
+}
+
+function showListCountries(countries) {
+  clearContent();
+  const markup = markupCountriesList(countries);
+  countriesListEl.innerHTML = markup;
+}
+
+function showMarkupCountry(country) {
+  clearContent();
+  const markup = renderMarkupCountryDescription(country);
+  countryInfoEl.innerHTML = markup;
+}
+
+function clearContent() {
+  countriesListEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+}
